7ffa5d7e9d4c06f07592b5cd7869f562df166164
Futaba Tomoyasu <t183364@im139.mac.network>
2018-10-09 16:25:41 +0900

    make files

---
+++ Practice1/Kadai1.java
@@ -0,0 +1,22 @@
+/*
+2018/10/9
+Problem 1
+Extraction of two or more Katakana words in a file and printing of frequent occurrences
+*/
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundExecption;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Kadai1{
+	
+	public static void main(String[] args){
+
+	}
+}
---
+++ test/practice1.java
@@ -0,0 +1,11 @@
+// practice1's program
+import java.io.*;
+
+public class practice1{
+	public static void main(String[] arg){
+		int a = 17 - 2 * ('H'-'A') << 1 + 8 % 5 / 2;
+		int x = 0x7f - ('Z'-'A'>>>1);
+		System.out.println(a);
+		System.out.println(x);
+	}
+}
